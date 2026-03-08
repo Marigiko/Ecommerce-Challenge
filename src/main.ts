8d3e723e93137dcd3d3b526d3833bf84e11339df
KeyzDev <86132907+Marigiko@users.noreply.github.com>
2026-03-08 16:54:12 -0300

    CORS enabled

--- src/main.ts
+++ src/main.ts
@@ -6,6 +6,8 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
+  // Enable CORS for all origins
+  // This need to be configured properly in production to allow only trusted origins
   app.enableCors();
 
   app.useGlobalPipes(new ValidationPipe({ transform: true }));
